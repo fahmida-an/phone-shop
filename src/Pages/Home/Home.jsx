e2c0daa676c9ed9ccef53fc43e113f5395a97039
Fahmida Khan <fahmidakhann.19@gmail.com>
2023-09-23 21:50:14 +0600

    Added phones dynamically

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,14 @@
-
+import Banner from "../../Components/Header/Banner/Banner";
+import Phones from "../../Components/Phones/Phones";
+import { useLoaderData } from "react-router-dom";
 const Home = () => {
+    const phones = useLoaderData();
+    // console.log(phones);
     return (
         <div>
-            
+            <Banner></Banner>
+            <Phones phones={phones}></Phones>
+          
         </div>
     );
 };
