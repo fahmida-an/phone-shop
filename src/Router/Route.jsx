1ee7477fc35bc458919a06e56203f9be4412e0c8
Fahmida Khan <fahmidakhann.19@gmail.com>
2023-09-23 21:09:28 +0600

    designed nav bar and login page

--- src/Router/Route.jsx
+++ src/Router/Route.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
+import Favourites from "../Pages/Favourites/Favourites";
+import Login from "../Pages/Login/Login";
 
 const myCreatedRoute = createBrowserRouter([
     {
@@ -10,10 +12,18 @@ const myCreatedRoute = createBrowserRouter([
             {
                 path: "/",
                 element: <Home></Home>
+            },
+            {
+                path: "/favourites",
+                element: <Favourites></Favourites>
+            },
+            {
+                path: "/login",
+                element: <Login></Login>
             }
         ]
 
     }
 ])
 
-export default Route;
+export default myCreatedRoute;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 
 import './index.css'
+import myCreatedRoute from './Router/Route'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={}></RouterProvider>
+    <RouterProvider router={myCreatedRoute}></RouterProvider>
   </React.StrictMode>,
 )
