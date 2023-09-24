e48850d3e823570ce098328d7f7ece3f944acc18
Fahmida Khan <fahmidakhann.19@gmail.com>
2023-09-24 22:35:29 +0600

    added see more button with conditions

--- src/Pages/Favourites/Favourites.jsx
+++ src/Pages/Favourites/Favourites.jsx
@@ -7,6 +7,7 @@ const Favourites = () => {
 
     const [favourites, setFavourites] = useState([]);
     const [noFound, setNoFound] =useState(false)
+    const [isShow, setIsShow] = useState(false)
 
     useEffect(()=>{
         const favouriteItems = JSON.parse(localStorage.getItem('favourites'));
@@ -34,11 +35,19 @@ const Favourites = () => {
                     }
                         <div className="grid grid-cols-2 gap-2">
                             {
-                                favourites.map(phone => <PhoneCard
+                                isShow ? 
+                                favourites.map((phone => <PhoneCard
                                 key ={phone.id} phone={phone}
-                                ></PhoneCard>)
+                                ></PhoneCard>))
+                                :
+                                favourites.slice(0,4).map((phone => <PhoneCard
+                                    key ={phone.id} phone={phone}
+                                    ></PhoneCard>))
                             }
                         </div>
+                        {
+                        favourites.length > 4 && <button onClick={()=> setIsShow(!isShow)} className="px-5 bg-green-200 block mx-auto">{isShow ? "See less" : "See more"}</button>
+}
                 </div>
             }
         </div>
