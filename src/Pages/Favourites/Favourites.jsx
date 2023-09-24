d6e11542a90682a99ac51af332966637e2d10fa7
Fahmida Khan <fahmidakhann.19@gmail.com>
2023-09-24 22:11:11 +0600

    Added delete button for favourites

--- src/Pages/Favourites/Favourites.jsx
+++ src/Pages/Favourites/Favourites.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import PhoneCard from "../Phone/PhoneCard"
+
+import PhoneCard from "../Phone/PhoneCard";
 
 
 const Favourites = () => {
@@ -16,13 +17,22 @@ const Favourites = () => {
         setNoFound('No Data Found !')
        }
     },[]);
+
     console.log(favourites);
+    const handleRemove =() =>{
+        localStorage.clear();
+        setFavourites([]);
+        setNoFound('No Data Found !')
+    }
 
     return (
         <div>{ noFound ? <p className="h-[80vh flex items-center justify-center" >{noFound}</p> 
                 : 
                 <div>
-                        <div>
+                    {
+                        favourites.length > 0 && <button onClick={handleRemove} className="px-5 bg-green-200 block mx-auto">Delete All favourites</button>
+                    }
+                        <div className="grid grid-cols-2 gap-2">
                             {
                                 favourites.map(phone => <PhoneCard
                                 key ={phone.id} phone={phone}
